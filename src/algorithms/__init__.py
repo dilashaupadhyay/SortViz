d803fd460e502eb835116c40a76360725812c478
dilashaupadhyay <dilashaupa@gmail.com>
2024-06-05 23:50:58 +0545

    algorithms added

---
+++ src/algorithms/__init__.py
@@ -0,0 +1,22 @@
+from algorithms.bubbleSort import bubbleSort
+from algorithms.binaryinsertionSort import binaryinsertionSort
+from algorithms.heapSort import heapSort
+from algorithms.countingSort import countingSort
+from algorithms.selectionSort import selectionSort
+from algorithms.quickSort import quickSort
+from algorithms.mergeSort import mergeSort
+from algorithms.insertionSort import insertionSort
+from algorithms.treeSort import treeSort
+
+
+__all__ = [
+    "binaryinsertionSort",
+    "bubbleSort",
+    "heapSort",
+    "countingSort",
+    "selectionSort",
+    "quickSort",
+    "mergeSort",
+    "insertionSort",
+    "treeSort"
+]
---
+++ src/algs.py
@@ -0,0 +1,13 @@
+from algorithms import *
+
+
+algorithmsDict = {'insertion'       : insertionSort,
+                  'bubble'          : bubbleSort,
+                  'selection'       : selectionSort,
+                  'merge'           : mergeSort,
+                  'quick'           : quickSort,
+                  'counting'        : countingSort,
+                  'heap'            : heapSort,
+                  'binary insertion': binaryinsertionSort,
+                  'tree'            : treeSort}
+
